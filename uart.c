ec1c25c19150364e83bf671fb86ff353b7c4a270
Hiroki Mori <yamori813@yahoo.co.jp>
2018-01-19 19:44:12 +0900

    only interrupt disable

--- uart.c
+++ uart.c
@@ -8,12 +8,6 @@
 
 void uart_init()
 {
-	/* 8 bits, 1 stop bit, no parity. */
-	REG32(UART_LCR_REG) = 0x03000000;
-
-	/* Reset/Enable the FIFO */
-	REG32(UART_FCR_REG) = 0xc7000000;
-
 	/* Disable All Interrupts */
 	REG32(UART_IER_REG) = 0x00000000;
 }
