b81be925f4f55d6c6e129c32d3170741fd9bff33
Hiroki Mori <yamori813@yahoo.co.jp>
2018-04-16 19:38:06 +0900

    delete not need line

--- hoge.rb
+++ hoge.rb
@@ -1,16 +1,12 @@
 #
 # rtlbm-mruby mruby script
 #
-# echo -n "GREEN" | nc -w 0 -u 10.10.10.2 7000
-#
 
 begin
 
 rtl = RTL8196C.new(RTL8196C::MODULE_HOMESPOTCUBE)
 
-rtl.udpbind(7000)
-
-rtl.print "Hello BearMetal mruby!!"
+rtl.print "Hello Bear Metal mruby!!"
 
 while 1 do
    rtl.print "."
