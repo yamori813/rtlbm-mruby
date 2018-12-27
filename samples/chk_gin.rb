9bdd117ca4c79e4f4f9952c07a298a05a1b16bc1
yamori813 <yamori813@yahoo.co.jp>
2018-12-27 13:20:52 +0900

    add gpio script

---
+++ samples/chk_gin.rb
@@ -0,0 +1,38 @@
+#
+# rtlbm-mruby mruby script
+# GPIO input check script for RTL8196E
+
+def delay(rtl, val) 
+  start = rtl.count() 
+  while rtl.count() < start + val do
+  end
+end
+
+begin
+
+rtl = YABM.new(YABM::MODULE_RTL8196E)
+
+# JTAG and all LED is GPIO
+rtl.gpiosetsel(0x06, 0x1ffffff, 0x36db, 0x3fff)
+
+# all pin is gpio
+reg = 0
+rtl.gpiosetctl(reg)
+
+#all pin is in
+reg = 0
+rtl.gpiosetdir(reg)
+
+rtl.print "GPIO input check\n"
+
+i = 0
+while 1 do
+  val = rtl.gpiogetdat()
+  rtl.print val.to_s + "\n"
+  delay(rtl, 2000)
+end
+
+rescue => e
+  rtl.print e.to_s
+end
+
---
+++ samples/chk_gout.rb
@@ -0,0 +1,49 @@
+#
+# rtlbm-mruby mruby script
+# GPIO output check script for RTL8196E
+
+def delay(rtl, val) 
+  start = rtl.count() 
+  while rtl.count() < start + val do
+  end
+end
+
+begin
+
+rtl = YABM.new(YABM::MODULE_RTL8196C)
+
+rtl.netstartdhcp
+
+# JTAG and all LED is GPIO
+rtl.gpiosetsel(0x06, 0x1ffffff, 0x36db, 0x3fff)
+
+# all pin is gpio
+reg = 0
+rtl.gpiosetctl(reg)
+
+#all pin is out
+reg = 0xffff
+rtl.gpiosetdir(reg)
+
+rtl.print "GPIO out check\n"
+
+i = 0
+while 1 do
+  rtl.print i.to_s + "\n"
+  if i < 16 
+    reg = 1 << i
+  else
+    reg = ~(1 << (i - 16))
+  end
+  rtl.gpiosetdat(reg)
+  delay(rtl, 2000)
+  i = i + 1
+  if i == 32
+    i = 0
+  end
+end
+
+rescue => e
+  rtl.print e.to_s
+end
+
