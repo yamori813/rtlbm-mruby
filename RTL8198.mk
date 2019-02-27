756b80ec6d1f18be0adf320f9226102bb5e39bca
yamori813 <yamori813@yahoo.co.jp>
2019-02-27 11:20:48 +0900

    revert option because of paformance issue

--- RTL8198.mk
+++ RTL8198.mk
@@ -5,6 +5,8 @@
 CROSS_CFLAGS += -DCONFIG_RTL8198 -DCONFIG_RTL8198_REVISION_B
 CROSS_CFLAGS += -DRTL8196=1 -DRTL8198=1
 CROSS_CFLAGS += -march=5281
-CROSS_LDFLAGS += -Lrsdk/mips-linux/lib/gcc/mips-linux/4.4.5-1.5.5p4/5281/
+# This is correct. But 4181 lib is fister than 5281 lib in fib() by mruby. ???
+#CROSS_LDFLAGS += -Lrsdk/mips-linux/lib/gcc/mips-linux/4.4.5-1.5.5p4/5281/
+CROSS_LDFLAGS += -Lrsdk/mips-linux/lib/gcc/mips-linux/4.4.5-1.5.5p4/4181/
 OBJS += swCore.o
 VMOBJ = main_8198
