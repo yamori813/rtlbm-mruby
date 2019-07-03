04eaecab160ba0d9b0e6e95ad03d33eab1c20ea5
yamori813 <yamori813@yahoo.co.jp>
2019-07-03 15:15:05 +0900

    support some ip address

--- flash.sh
+++ flash.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+
+if [ $# -eq 2 -a $1 == "-2" ]; then
+IMG=$2
+IPADDR=192.168.2.1
+elif [ $# -eq 2 -a $1 == "-0" ]; then
+IMG=$2
+IPADDR=192.168.0.1
+elif [ $# -eq 1 ]; then
 IMG=$1
+IPADDR=192.168.1.6
+else
+echo "flash.sh [-02] <img file>"
+exit
+fi
+
+echo "target is ${IPADDR}"
 
 PATH=`/usr/bin/dirname ${IMG}`
 FILE=`/usr/bin/basename ${IMG}`
 
 cd ${PATH} ; echo "bin
 put ${FILE}
-quit" | /usr/bin/tftp 192.168.1.6
+quit" | /usr/bin/tftp ${IPADDR}
