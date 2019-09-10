6af2a6c4297ca36bb8fe6e554b576a9fa8c12d77
yamori813 <yamori813@yahoo.co.jp>
2019-09-10 23:49:54 +0900

    add error check and more

--- samples/bmp180.rb
+++ samples/bmp180.rb
@@ -4,12 +4,24 @@
 
 APIKEY = "naisyo"
 
-BMPADDR = 0x77
+# Homespotcube GPIO
 
-# GPIO I2C Pin
+STATUS_LED1 = (1 << 0)
+STATUS_LED2 = (1 << 6)
+STATUS_LED3 = (1 << 17)
+TOP_LED1 = (1 << 16)
+TOP_LED2 = (1 << 4)
+TOP_LED3 = (1 << 1)
+TOP_BUTTON = (1 << 3)
+
+# GPIO I2C Pin (SW12)
 
 SCL = 2
 SDA = 11
+
+BMPADDR = 0x77
+
+PRESSURE_WAIT = [5, 8, 14, 26]
 
 def readup(yabm, oss) 
   msb = yabm.i2cread(BMPADDR, 0xf6)
@@ -51,6 +63,24 @@ begin
 
   yabm.gpiosetsel(0x003c300c, 0x003c300c, 0x00001800, 0x00001800)
 
+  reg = yabm.gpiogetctl()
+  reg = reg & ~(STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
+  reg = reg & ~(TOP_LED1 | TOP_LED2 | TOP_LED3)
+  reg = reg & ~(TOP_BUTTON)
+  yabm.gpiosetctl(reg)
+
+  reg = yabm.gpiogetdir()
+  reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
+  reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
+  reg = reg & ~(TOP_BUTTON)
+  yabm.gpiosetdir(reg)
+
+  reg = yabm.gpiogetdat()
+  reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
+  reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
+  yabm.gpiosetdat(reg)
+
+
   yabm.i2cinit(SCL, SDA)
 
   ac1 = read16(yabm, 0xaa)
@@ -67,66 +97,83 @@ begin
 
 # 0 ultra low power, 1 standard, 2 high resolution, 3 ultra high resolution
 
-  oss = 1
+  oss = 3
 
   while 1 do
+
+    reg = yabm.gpiogetdat()
+    reg = reg & ~TOP_LED3
+    yabm.gpiosetdat(reg)
+
+    yabm.gpiosetsel(0x003c300c, 0x003c300c, 0x00001800, 0x00001800)
+    yabm.i2cinit(SCL, SDA)
+
     yabm.i2cwrite(BMPADDR, 0xf4, 0x2e)
     delay(yabm, 5)
     ut = read16(yabm, 0xf6)
+    yabm.print ut.to_s + "\n"
 
     yabm.i2cwrite(BMPADDR, 0xf4, 0x34 + (oss << 6))
-# depend on oss
-    delay(yabm, 8)
+    delay(yabm, PRESSURE_WAIT[oss])
     up = readup(yabm, oss)
+    yabm.print up.to_s + "\n"
+
+    if ut != 0 && up != 0 then
 
 # calculate true temperature
 
-    x1 = (ut - ac6) * ac5 >> 15
-    x2 = (mc << 11) / (x1 + md)
-    b5 = x1 + x2
-    t = (b5 + 8) >> 4
+      x1 = (ut - ac6) * ac5 >> 15
+      x2 = (mc << 11) / (x1 + md)
+      b5 = x1 + x2
+      t = (b5 + 8) >> 4
 
-    tstr = (t / 10).to_s + "." + (t % 10).to_s
+      tstr = (t / 10).to_s + "." + (t % 10).to_s
 
-    yabm.print tstr + "\n"
+      yabm.print tstr + "\n"
 
 # calculate true pressure
 
-    b6 = b5 - 4000
-    x1 = (b2 * (b6 * b6 >> 12)) >> 11
-    x2 = ac2 * b6 >> 11
-    x3 = x1 + x2
-    b3 = (((ac1 * 4 + x3) << oss) + 2) >> 2
-    x1 = ac3 * b6 >> 13
-    x2 = (b1 * (b6 * b6 >> 12)) >> 16
-    x3 = ((x1 + x2) + 2) >> 2
-    b4 = (ac4 * (x3 + 32768)) >> 15
-    b7 = (up - b3) * (50000 >> oss)
+      b6 = b5 - 4000
+      x1 = (b2 * (b6 * b6 >> 12)) >> 11
+      x2 = ac2 * b6 >> 11
+      x3 = x1 + x2
+      b3 = (((ac1 * 4 + x3) << oss) + 2) >> 2
+      x1 = ac3 * b6 >> 13
+      x2 = (b1 * (b6 * b6 >> 12)) >> 16
+      x3 = ((x1 + x2) + 2) >> 2
+      b4 = (ac4 * (x3 + 32768)) >> 15
+      b7 = (up - b3) * (50000 >> oss)
 
 # This is only temporally code because of valid non overflow
-    p = b7 / (b4 >> 1)
+      p = b7 / (b4 >> 1)
 
-    x1 = (p >> 8) * (p >> 8)
-    x1 = (x1 * 3038) >> 16
-    x2 = (-7357 * p) >> 16
-    p = p + ((x1 + x2 + 3791) >> 4)
+      x1 = (p >> 8) * (p >> 8)
+      x1 = (x1 * 3038) >> 16
+      x2 = (-7357 * p) >> 16
+      p = p + ((x1 + x2 + 3791) >> 4)
 
-    pstr = (p / 100).to_s + "."
-    syo = p % 100
-    if syo < 10 then
-      pstr = pstr + "0" + syo.to_s
-    else
-      pstr = pstr + syo.to_s
+      pstr = (p / 100).to_s + "."
+      syo = p % 100
+      if syo < 10 then
+        pstr = pstr + "0" + syo.to_s
+      else
+        pstr = pstr + syo.to_s
+      end
+      yabm.print pstr + "\n"
+
+      res = SimpleHttp.new("https", "api.thingspeak.com", 443).request("GET", "/update?api_key=" + APIKEY + "&field1=" + tstr + "&field2=" + pstr, {'User-Agent' => "test-agent"})
+      if res 
+        yabm.print " " + res.status.to_s
+      end
+      yabm.print "\n"
+
     end
-    yabm.print pstr + "\n"
 
-    res = SimpleHttp.new("https", "api.thingspeak.com", 443).request("GET", "/update?api_key=" + APIKEY + "&field1=" + tstr + "&field2=" + pstr, {'User-Agent' => "test-agent"})
-    if res 
-      yabm.print " " + res.status.to_s
-    end
-    yabm.print "\n"
+    reg = yabm.gpiogetdat()
+    reg = reg | TOP_LED3
+    yabm.gpiosetdat(reg)
 
-    delay(yabm, 30000)
+    delay(yabm, 10000)
 
   end
 
