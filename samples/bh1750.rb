31553930adc26406615bfca892782931e477f7ad
yamori813 <yamori813@yahoo.co.jp>
2020-05-12 08:50:35 +0900

    make class

--- samples/bh1750.rb
+++ samples/bh1750.rb
@@ -8,9 +8,9 @@ APIKEY = "naisyo"
 
 # i2c pin
 
-# TRST# (1) internal pull-up resistor
+# TRST# (1) has internal pull-up resistor
 I2CSCK = 3
-# TDI (3) internal pull-up resistor
+# TDI (3) has internal pull-up resistor
 I2CSDA = 5
 
 # i2c BH1750 address
@@ -20,6 +20,72 @@ BHADDR = 0x23
 def delay(yabm, val) 
   start = yabm.count() 
   while yabm.count() < start + val do
+  end
+end
+
+class BH1750
+  @mtreg = 69
+
+  # Measurement at 1 lux resolution. Measurement time is approx 120ms.
+  CONTINUOUS_HIGH_RES_MODE  = 0x10
+  # Measurement at 0.5 lux resolution. Measurement time is approx 120ms.
+  CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+  # Measurement at 4 lux resolution. Measurement time is approx 16ms.
+  CONTINUOUS_LOW_RES_MODE = 0x13
+  # Measurement at 1 lux resolution. Measurement time is approx 120ms.
+  ONE_TIME_HIGH_RES_MODE = 0x20
+  # Measurement at 0.5 lux resolution. Measurement time is approx 120ms.
+  ONE_TIME_HIGH_RES_MODE_2 = 0x21
+  # Measurement at 4 lux resolution. Measurement time is approx 16ms.
+  ONE_TIME_LOW_RES_MODE = 0x23
+
+  def init yabm, addr
+    @y = yabm
+    @addr = addr
+  end
+
+  def setMTreg mtreg
+    @mtreg = mtreg
+    @y.i2cwrites(@addr, [0x40 | (@mtreg >> 5)], 0)
+    delay(@y, 200)
+    @y.i2cwrites(@addr, [0x60 | (@mtreg & 0x1f)], 0)
+    delay(@y, 200)
+  end
+
+  def setMeasurement mode
+    @meas = mode
+    if @meas == CONTINUOUS_HIGH_RES_MODE ||
+      @meas == CONTINUOUS_HIGH_RES_MODE_2 ||
+      @meas == CONTINUOUS_LOW_RES_MODE then
+      @y.i2cwrites(@addr, [@meas], 0)
+    end
+  end
+
+  def getLightLevel
+    if @meas == ONE_TIME_HIGH_RES_MODE ||
+      @meas == ONE_TIME_HIGH_RES_MODE_2 then
+      @y.i2cwrites(@addr, [@meas], 0)
+      delay(@y, 120 * @mtreg / 69)
+    elsif @meas == ONE_TIME_LOW_RES_MODE then
+      @y.i2cwrites(@addr, [@meas], 0)
+      delay(@y, 16 * @mtreg / 69)
+    end
+    bharr = @y.i2creads(@addr, 2)
+    val = (bharr[0] << 8) | bharr[1]
+    if @meas == CONTINUOUS_HIGH_RES_MODE_2 ||
+      @meas == ONE_TIME_HIGH_RES_MODE_2 then
+      lx = (val * 50 * 69 * 5 / (6 * @mtreg)).round
+    else
+      lx = (val * 100 * 69 * 5 / (6 * @mtreg)).round
+    end
+    if lx == 0 then
+      lxstr = "0"
+    elsif lx < 100 then
+      lxstr = "0." + lx.to_s
+    else
+      lxstr = lx.to_s.insert(-3, ".")
+    end
+    return lxstr
   end
 end
 
@@ -42,35 +108,30 @@ yabm.gpiosetdat(gpio | (1 << 16) | 0x7c00)
 
 yabm.i2cinit(I2CSCK, I2CSDA, 1)
 
-mtreg = 254
+bh = BH1750.new
 
-yabm.i2cwrites(BHADDR, [0x40 | (mtreg >> 5)], 0)
-delay(yabm, 200)
-yabm.i2cwrites(BHADDR, [0x60 | (mtreg & 0x1f)], 0)
-delay(yabm, 200)
-yabm.i2cwrites(BHADDR, [0x11], 0)
-delay(yabm, 200)
+bh.init(yabm, BHADDR)
+
+mtreg = 254
+mode = BH1750::CONTINUOUS_HIGH_RES_MODE_2
+
+interval = 20
+
+bh.setMTreg(mtreg)
+
+bh.setMeasurement(mode)
 
 count = 0
+
 while 1 do
-  yabm.print "."
-  bharr = yabm.i2creads(BHADDR, 2)
-  val = (bharr[0] << 8) | bharr[1]
-  lx = (val * 50 * 69 * 5 / (6 * mtreg)).round
-  if lx == 0 then
-    lxstr = "0"
-  elsif lx < 100 then
-    lxstr = "0." + lx.to_s
-  else
-    lxstr = lx.to_s.insert(-3, ".")
-  end
+  delay(yabm, 1000 * interval)
+  lxstr = bh.getLightLevel
   para = "api_key=" + APIKEY + "&field1=" + count.to_s + "&field2=" + lxstr
   res = SimpleHttp.new("https", "api.thingspeak.com", 443).request("GET", "/update?" + para, {'User-Agent' => "test-agent"})
   if res != nil && res.status.to_s.length != 0 then
     yabm.print count.to_s + " " + lxstr + " " + res.status.to_s + "\r\n"
   end
   count = count + 1
-  delay(yabm, 1000 * 20)
 end
 
 rescue => e
