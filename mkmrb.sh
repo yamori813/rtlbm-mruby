5cf75d4306d539c850e87c46f1d350cdb22eb432
hiroki <hiroki@debian>
2021-01-11 15:57:33 +0900

    mrbc path change

--- mkmrb.sh
+++ mkmrb.sh
@@ -4,6 +4,6 @@ TARGET=`echo $1 | sed 's/\.rb//'`
 
 MRBOFFSET=0x180000
 
-./mruby/build/host/bin/mrbc ${TARGET}.rb
+./mruby/build/host/mrbc/bin/mrbc ${TARGET}.rb
 
 ./rtktools/cvimg root ${TARGET}.mrb ${TARGET}.img ${MRBOFFSET} ${MRBOFFSET}
