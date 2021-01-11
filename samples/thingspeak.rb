3649177ace155d2428857a3b393edc0688488069
hiroki <hiroki@debian>
2021-01-11 16:03:42 +0900

    add ipv6 support by comment

--- samples/thingspeak.rb
+++ samples/thingspeak.rb
@@ -42,20 +42,25 @@ yabm.gpiosetdat(reg | ALLLED)
 ntpaddr = yabm.lookup("ntp.nict.jp")
 yabm.sntp(ntpaddr)
 
+#start = yabm.count()
+#while yabm.count() < start + 3 * 1000 do
+#end
+#ntpaddr6 = yabm.lookup6("ntp.nict.jp")
+#yabm.print ntpaddr6 + "\r\n"
+#yabm.sntp(ntpaddr6)
+
 count = 0
-interval = 15
+interval = 30
 
 yabm.watchdogstart(256)
 
 while 1 do
   count = count + 1
-  start = yabm.count()
-  while yabm.count() < start + interval * 1000 do
-  end
   ledon yabm
-  yabm.print count.to_s + "\r\n"
+  yabm.print count.to_s
   res = SimpleHttp.new("https", "api.thingspeak.com", 443).request("GET", "/update?api_key=" + APIKEY + "&field1=" + count.to_s, {'User-Agent' => "test-agent"})
   ledoff yabm
+  yabm.print "." + "\r\n"
   start = yabm.count()
   while yabm.count() < start + interval * 1000 do
   end
