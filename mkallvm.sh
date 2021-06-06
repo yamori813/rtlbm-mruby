ece7646ec4a24441b320cb5045dd160a0a324054
Hiroki Mori <yamori813@yahoo.co.jp>
2021-06-07 06:46:55 +0900

    add build script

---
+++ mkallvm.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TOOLPATH=$HOME/rtl819x-toolchain/toolchain/rsdk-1.5.5-5281-EB-2.6.30-0.9.30.3-110714
+PATH=${PATH}:${TOOLPATH}/mips-linux/bin:${TOOLPATH}/libexec/gcc/mips-linux/4.4.5-1.5.5p4
+
+START=`date '+%s'`
+
+rm *rtl
+make clean
+make TERGET=RTL8196E
+mv main.rtl main_e.rtl
+make clean
+make TERGET=RTL8197D_SW
+mv main.rtl main_8197d_sw.rtl
+make clean
+make TERGET=RTL8198
+mv main.rtl main_8198.rtl
+make clean
+make
+
+
+END=`date '+%s'`
+
+TIME=`expr ${END} - ${START}`
+
+echo ${TIME}
