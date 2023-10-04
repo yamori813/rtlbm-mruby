d3cd552a0cbc972e6ddd4955b9e1e91d598a1409
Hiroki Mori <yamori813@yahoo.co.jp>
2023-10-04 11:32:22 +0900

    use hash

--- samples/udpled.rb
+++ samples/udpled.rb
@@ -5,21 +5,26 @@
 # need sub_hsc.rb
 #
 
+sarr = [0, STATUS_LED1, STATUS_LED2, STATUS_LED3]
+tarr = {
+  'GREEN' => TOP_LED2,
+  'RED' => TOP_LED1,
+  'ORANGE' => TOP_LED3,
+  'OFF' => 0}
+
 def button(rtl)
   reg = rtl.gpiogetdat()
   return reg & TOP_BUTTON
 end
 
 def led(rtl, type, val)
-  sarr = [0, STATUS_LED1, STATUS_LED2, STATUS_LED3]
-  tarr = [0, TOP_LED1, TOP_LED2, TOP_LED3]
   reg = rtl.gpiogetdat()
   if(type == 1) then
     reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-    reg = reg & ~sarr[val]
+    reg = reg & ~val
   else
     reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
-    reg = reg & ~tarr[val]
+    reg = reg & ~val
   end
   rtl.gpiosetdat(reg)
 end
@@ -43,31 +48,23 @@ gpioinit(rtl)
 stat = 0
 lastbtn = button(rtl)
 
-i = 0
 loop do
    udpstr = rtl.udprecv()
-   if udpstr.length != 0 then
-     if udpstr == "GREEN" then
-       led(rtl, 2, 2)
-     elsif udpstr == "RED" then
-       led(rtl, 2, 1)
-     elsif udpstr == "ORANGE" then
-       led(rtl, 2, 3)
-     elsif udpstr == "OFF" then
-       led(rtl, 2, 0)
-     end
-     rtl.print udpstr
-   end
    if lastbtn != button(rtl) && lastbtn != 0 then
      rtl.print "button"
      stat = stat + 1
      if stat == 4 then
        stat = 0
      end
-     led(rtl, 1, stat)
+     led(rtl, 1, sarr[stat])
+   end
+   if udpstr.length != 0 then
+     if tarr.has_key?(udpstr) then
+       led(rtl, 2, tarr[udpstr])
+     end
+     rtl.print udpstr
    end
    lastbtn = button(rtl)
-   i = i + 1
 end
 
 rescue => e
