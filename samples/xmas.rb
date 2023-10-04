07bde9c185eab41864862092ecd9081b81692719
Hiroki Mori <yamori813@yahoo.co.jp>
2023-10-04 12:22:19 +0900

    use subroutine

--- samples/xmas.rb
+++ samples/xmas.rb
@@ -1,34 +1,7 @@
 #
 # rtlbm-mruby mruby script
+# need sub_hsc.rb
 #
-# T2
-# 1	VCC
-# 2	96	GPIOA[3]	SW
-# 3	43	GPIOC[1]	Status LED
-# 4	109	GPIOC[0]	TOP LED
-# 5	98	GPIOA[6]	Status LED
-# 6	97	GPIOA[4]	TOP LED
-# 7	67	GPIOA[0]	Status LED
-# 8	40	GPIOA[1]	TOP LED
-# 9	GND
-# 10	GND
-# SW12
-# 	100	GPIOA[2]
-#	102	GPIOB[3]
-# Reset
-#	99	GPIOA[5]
-#
-
-STATUS_LED1 = (1 << 0)
-STATUS_LED2 = (1 << 6)
-STATUS_LED3 = (1 << 17)
-TOP_LED1 = (1 << 16)
-TOP_LED2 = (1 << 4)
-TOP_LED3 = (1 << 1)
-TOP_BUTTON = (1 << 3)
-SW1 = (1 << 2)
-SW2 = (1 << 11)
-
 
 def switch(rtl)
 
@@ -61,29 +34,6 @@ def led(rtl, type, val)
   rtl.gpiosetdat(reg)
 end
 
-def initgpio(rtl)
-
-  rtl.gpiosetsel(0x003c300c, 0x003c300c, 0x00001800, 0x00001800)
-
-  reg = rtl.gpiogetctl()
-  reg = reg & ~(STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-  reg = reg & ~(TOP_LED1 | TOP_LED2 | TOP_LED3)
-  reg = reg & ~(TOP_BUTTON)
-  rtl.gpiosetctl(reg)
-
-  reg = rtl.gpiogetdir()
-  reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-  reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
-  reg = reg & ~(TOP_BUTTON)
-  rtl.gpiosetdir(reg)
-
-  reg = rtl.gpiogetdat()
-  reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-  reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
-  rtl.gpiosetdat(reg)
-
-end
-
 begin
 
   rtl = YABM.new
@@ -91,7 +41,7 @@ begin
   ontime = 2000
   offtime = 500
 
-  initgpio(rtl)
+  gpioinit(rtl)
 
   lastbtn = button(rtl)
 
