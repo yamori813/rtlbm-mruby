5cdaa962249443accd600d82f1ccefcb118acad7
Hiroki Mori <yamori813@yahoo.co.jp>
2023-10-04 10:40:07 +0900

    use subroutine

--- samples/udpled.rb
+++ samples/udpled.rb
@@ -1,33 +1,9 @@
 #
 # rtlbm-mruby mruby script
 #
-# T2
-# 1	VCC
-# 2	96	GPIOA[3]	SW
-# 3	43	GPIOC[1]	Status LED
-# 4	109	GPIOC[0]	TOP LED
-# 5	98	GPIOA[6]	Status LED
-# 6	97	GPIOA[4]	TOP LED
-# 7	67	GPIOA[0]	Status LED
-# 8	40	GPIOA[1]	TOP LED
-# 9	GND
-# 10	GND
-# SW12
-# 	100	GPIOA[2]
-#	102	GPIOB[3]
-# Reset
-#	99	GPIOA[5]
-#
 # echo -n "GREEN" | nc -w 0 -u 10.10.10.2 7000
+# need sub_hsc.rb
 #
-
-STATUS_LED1 = (1 << 0)
-STATUS_LED2 = (1 << 6)
-STATUS_LED3 = (1 << 17)
-TOP_LED1 = (1 << 16)
-TOP_LED2 = (1 << 4)
-TOP_LED3 = (1 << 1)
-TOP_BUTTON = (1 << 3)
 
 def button(rtl)
   reg = rtl.gpiogetdat()
@@ -62,24 +38,7 @@ rtl.netstart(addr, mask, gw, dns)
 rtl.udpinit
 rtl.udpbind(7000)
 
-rtl.gpiosetsel(0x003c300c, 0x003c300c, 0x00001800, 0x00001800)
-
-reg = rtl.gpiogetctl()
-reg = reg & ~(STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-reg = reg & ~(TOP_LED1 | TOP_LED2 | TOP_LED3)
-reg = reg & ~(TOP_BUTTON)
-rtl.gpiosetctl(reg)
-
-reg = rtl.gpiogetdir()
-reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
-reg = reg & ~(TOP_BUTTON)
-rtl.gpiosetdir(reg)
-
-reg = rtl.gpiogetdat()
-reg = reg | (STATUS_LED1 | STATUS_LED2 | STATUS_LED3)
-reg = reg | (TOP_LED1 | TOP_LED2 | TOP_LED3)
-rtl.gpiosetdat(reg)
+gpioinit(rtl)
 
 stat = 0
 lastbtn = button(rtl)
