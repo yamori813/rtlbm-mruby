8d7abff7a0f535be6c24c1b1b9f7c6766452a35e
Hiroki Mori <yamori813@yahoo.co.jp>
2024-07-17 10:12:36 +0900

    print sha256

--- mkmrb.sh
+++ mkmrb.sh
@@ -7,6 +7,8 @@ MRBOFFSET=0x180000
 
 ./mruby/build/host/bin/mrbc -o${TARGET}.mrb $*
 
+sha256 ${TARGET}.mrb
+
 ./rtktools/cvimg root ${TARGET}.mrb ${TARGET}.img ${MRBOFFSET} ${MRBOFFSET}
 
 echo "build ${TARGET}.img"
