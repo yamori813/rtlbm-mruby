703e686aa6ab854ae318c963512a45bb69a60848
Hiroki Mori <yamori813@yahoo.co.jp>
2025-12-05 16:10:43 +0900

    enlarge lwip MEM_SIZE

--- build/lwip/include/lwipopts.h
+++ build/lwip/include/lwipopts.h
@@ -73,7 +73,7 @@
  * MEM_SIZE: the size of the heap memory. If the application will send
  * a lot of data that needs to be copied, this should be set high.
  */
-#define MEM_SIZE                        16000
+#define MEM_SIZE                        64000
 
 
 /*
